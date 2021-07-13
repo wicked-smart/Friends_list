f487aa2a72c932c975d6aad156f67820966cdc12
wicked-smart <marzaanvan@gmail.com>
2021-07-13 21:08:31 +0530

    Initial Commit

---
+++ js/ajax.js
@@ -0,0 +1,17 @@
+function cs50Info(name){
+  if(name == "")
+    return;
+
+  var ajax = new XMLHttpRequest();
+
+  ajax.onreadystatechange = function(){
+      if (ajax.readyState == 4 && ajax.status == 200){
+        $('#infodiv').html(ajax.responseText);
+      }
+  };
+
+  ajax.open('GET', name + '.html', true);
+  ajax.send();
+
+
+};
